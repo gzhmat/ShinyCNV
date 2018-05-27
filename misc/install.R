e2bf600210aca87a7c5f2ef36c751ace3699823e
gzhmat <gzhmat@gmail.com>
2018-05-26 20:10:17 -0500

    V1.0

--- misc/install.R
+++ misc/install.R
@@ -1,4 +1,4 @@
-list.of.packages = c("rstudioapi", "shiny", "magrittr", "scales", "dplyr", "data.table", "DT", "readr")
+list.of.packages = c("shiny", "magrittr", "scales", "dplyr", "data.table", "DT", "readr")
 new.packages = list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages))
   install.packages(new.packages)
